1b72b49c126c7a8e55b1976c8da5883498f07b4d
udodinho <udodinho200@gmail.com>
2024-07-24 13:44:22 +0100

    Add database migration

---
+++ src/HealthTracker.DataService/Migrations/20240624150910_Adding health data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthTracker.DataService.Migrations
+{
+    public partial class Addinghealthdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HealthData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Height = table.Column<decimal>(type: "numeric", nullable: false),
+                    Weight = table.Column<decimal>(type: "numeric", nullable: false),
+                    BloodType = table.Column<string>(type: "text", nullable: false),
+                    Race = table.Column<string>(type: "text", nullable: false),
+                    UseGlasses = table.Column<bool>(type: "boolean", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HealthData", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HealthData");
+        }
+    }
+}
