8610193b33ceebcc95e429d0f424489dcdbad3dd
udodinho <udodinho200@gmail.com>
2024-08-08 11:07:43 +0100

    Add user login response dto

---
+++ src/HealthTracker.Authentication/Models/DTO/Outgoing/UserLoginResponseDto.cs
@@ -0,0 +1,8 @@
+
+using HealthTracker.Authentication.Models.DTO.Outgoing;
+
+namespace HealthTracker.Authentication.Model.DTO.Outgoing
+{
+    public class UserLoginResponseDto : AuthResult
+    {}
+}
