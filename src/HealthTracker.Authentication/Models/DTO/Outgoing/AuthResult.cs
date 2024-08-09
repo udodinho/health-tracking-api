d0a77441cd0cc40e5ca28ec8e6977223717e020f
udodinho <udodinho200@gmail.com>
2024-08-09 12:35:15 +0100

    Add auth result dto

---
+++ src/HealthTracker.Authentication/Models/DTO/Outgoing/AuthResult.cs
@@ -0,0 +1,11 @@
+
+namespace HealthTracker.Authentication.Models.DTO.Outgoing
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public bool Success { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
