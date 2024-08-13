599fbfc0f64871c9846950fb6ed5a232c0e9b7d3
udodinho <udodinho200@gmail.com>
2024-08-13 11:42:22 +0100

    Add paged result dto

---
+++ src/HealthTracker.Entities/Dtos/Generic/PagedResult.cs
@@ -0,0 +1,10 @@
+
+namespace HealtTracker.Entities.Dtos.Generic
+{
+    public class PageResult<T> : Result<List<T>>
+    {
+        public int Page { get; set; }
+        public int ResultCount { get; set; }
+        public int ResultsPerPage { get; set; }
+    }
+}
