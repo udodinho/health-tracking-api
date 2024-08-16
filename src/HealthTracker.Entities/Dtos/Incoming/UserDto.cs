905795c18370b8a217925eed86db8ac7aa8fdc18
udodinho <udodinho200@gmail.com>
2024-08-16 11:20:24 +0100

    Add user dto

---
+++ src/HealthTracker.Entities/Dtos/Incoming/UserDto.cs
@@ -0,0 +1,16 @@
+namespace HealtTracker.Entities.Dtos.Incoming
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string DateofBirth { get; set; }
+        public string Country { get; set; }
+         public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string Gender { get; set; }
+    }
+
+}
