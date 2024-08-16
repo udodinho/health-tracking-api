a727cdf3feb52802e38e4156ad7e9c6f5a05f690
udodinho <udodinho200@gmail.com>
2024-08-16 11:20:50 +0100

    Add update user dto

---
+++ src/HealthTracker.Entities/Dtos/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+
+namespace HealtTracker.Entities
+{
+    public class UpdateUserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string DateofBirth { get; set; }
+        public string Country { get; set; }
+    }
+
+}
