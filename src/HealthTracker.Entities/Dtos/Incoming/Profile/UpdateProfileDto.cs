9e53ec35885f11fa67674cc10f769d8ed311724c
udodinho <udodinho200@gmail.com>
2024-08-19 23:04:25 +0100

    Add update profile dto

---
+++ src/HealthTracker.Entities/Dtos/Incoming/Profile/UpdateProfileDto.cs
@@ -0,0 +1,11 @@
+
+namespace HealtTracker.Entities.Dtos.Incoming.Profile
+{
+   public class UpdateProfileDto
+    {
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string Gender { get; set; }
+    }    
+}
