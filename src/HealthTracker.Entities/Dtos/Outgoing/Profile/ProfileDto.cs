bbda1227c41f47aa8502c47d4a126c290b90a678
udodinho <udodinho200@gmail.com>
2024-08-19 23:04:15 +0100

    Add profile dto

---
+++ src/HealthTracker.Entities/Dtos/Outgoing/Profile/ProfileDto.cs
@@ -0,0 +1,12 @@
+namespace HealthTracker.Entities.Dtos.Outgoing.Profile
+{
+    public class ProfileDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string DateofBirth { get; set; }
+        public string Country { get; set; }
+    }
+}
