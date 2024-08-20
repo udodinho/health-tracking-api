ba776c1c7160ddd774c483d90a5079e7f6c77bc1
udodinho <udodinho200@gmail.com>
2024-08-20 11:21:05 +0100

    Add error dto

---
+++ src/HealthTracker.Entities/Dtos/Error/Errors.cs
@@ -0,0 +1,10 @@
+
+namespace HealtTracker.Entities.Dtos.Errors
+{
+    public class Error
+    {
+        public int Code { get; set; }
+        public string Type { get; set; }
+        public string Message { get; set; }
+    }
+}
